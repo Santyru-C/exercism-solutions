c31c172befff2eb1e0236b2d935eba73c561ba11
Santyru <santiago0988@hotmail.com>
2023-06-01 14:07:19 -0300

    add third solution

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,12 +10,28 @@
  *   ...
  * }
  */
+
+// not assigned default parameters evaluate to undefined
 export function cookingStatus(remainingTime) { //time evaluated in minutes
     if (remainingTime === 0) return "Lasagna is done."
     if (remainingTime === undefined) return "You forgot to set the timer."
     return "Not done, please wait."
 };
 
-export function preparationTime(layers, timePerLayer = 2){
+//default parameters
+export function preparationTime(layers, timePerLayer = 2){ 
     return layers.length * timePerLayer
 }
+
+export function quantities(layers) {
+    const SAUCE_PER_LAYER = 0.2 // litres
+    const NOODLES_PER_LAYER = 50 // grams
+    let sauce_layers = layers.filter((layer) => layer === "sauce").length
+    let noodle_layers = layers.filter((layer) => layer === "noodles").length
+    let sauce = SAUCE_PER_LAYER * sauce_layers
+    let noodles = NOODLES_PER_LAYER * noodle_layers
+
+    return { noodles, sauce }
+}
+
+
