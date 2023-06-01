53280b43f6f52d7e841a81c18781e3f744bf65f4
Santyru <santiago0988@hotmail.com>
2023-06-01 14:27:04 -0300

    add solution number 4

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -23,15 +23,21 @@ export function preparationTime(layers, timePerLayer = 2){
     return layers.length * timePerLayer
 }
 
+// entity for returning multiple information
 export function quantities(layers) {
-    const SAUCE_PER_LAYER = 0.2 // litres
-    const NOODLES_PER_LAYER = 50 // grams
-    let sauce_layers = layers.filter((layer) => layer === "sauce").length
-    let noodle_layers = layers.filter((layer) => layer === "noodles").length
-    let sauce = SAUCE_PER_LAYER * sauce_layers
-    let noodles = NOODLES_PER_LAYER * noodle_layers
-
+    const SAUCE_PER_LAYER = 0.2; // litres
+    const NOODLES_PER_LAYER = 50; // grams
+    let sauce_layers = layers.filter((layer) => layer === "sauce").length;
+    let noodle_layers = layers.filter((layer) => layer === "noodles").length;
+    let sauce = SAUCE_PER_LAYER * sauce_layers;
+    let noodles = NOODLES_PER_LAYER * noodle_layers;
     return { noodles, sauce }
+}
+
+// mutating non-primitive arguments
+export function addSecretIngredient(firstRecipe, secondRecipe) {
+    const SECRET_INGREDIENT = firstRecipe[firstRecipe.length -1];
+    if (SECRET_INGREDIENT) secondRecipe.push(SECRET_INGREDIENT);
 }
 
 
