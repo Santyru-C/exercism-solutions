c5908ecea27cb0c4ff8b719579a8011d03887c88
Santyru <santiago0988@hotmail.com>
2023-06-09 15:41:26 -0300

    add Position class

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -10,8 +10,20 @@ export function Size(width = 80, height = 60) {
     this.height = height
 }
 
-// add method via the prototype property of the constructor.
+// add method via the prototype property of the constructor to take
+// advantage of the prototype chain.
 Size.prototype.resize = function(newWidth, newHeight) {
     this.width = newWidth;
     this.height = newHeight;
-};
+}; // else every instance of the object would have inherited the method instead
+    // instead of just searching for it in its prototype. (it ends requiring more memory)
+
+export function Position(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+}
+
+Position.prototype.move = function(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+}
