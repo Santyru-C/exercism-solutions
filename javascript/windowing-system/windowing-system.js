4a7b091143bb54ed8209a72cbeeebb6433135da1
Santyru <santiago0988@hotmail.com>
2023-06-10 14:51:12 -0300

    add method to ProgramWindow

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -40,12 +40,26 @@ export class ProgramWindow {
             size.resize(1, 1)
         }
         if (size.width + this.position.x > this.screenSize.width) {
-            size.width -= this.screenSize.width - this.position.x 
+            size.width = this.screenSize.width - this.position.x
         }
         if (size.height + this.position.y > this.screenSize.height) {
-            size.height -= this.screenSize.height - this.position.y
+            size.height = this.screenSize.height - this.position.y
         }
-        this.size = size
+        this.size = size;
+    }
+
+    move(newPosition) {
+        if (newPosition.x < 0 || newPosition.y < 0) {
+            newPosition.move(0, 0)
+        }
+        if (newPosition.x + this.size.width > this.screenSize.width) {
+            newPosition.x = this.screenSize.width - this.size.width
+        }
+        if (newPosition.y + this.size.height > this.screenSize.height) {
+            newPosition.y = this.screenSize.height - this.size.height
+        }
+
+        this.position = newPosition
     }
 }
 
